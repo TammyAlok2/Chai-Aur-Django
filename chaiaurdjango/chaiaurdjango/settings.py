a304447fcb27e6e3d2c0c12db17725828bef494c
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-06-08 13:56:07 +0530

    half tailwind configuration done

--- chaiaurdjango/chaiaurdjango/settings.py
+++ chaiaurdjango/chaiaurdjango/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chai',
+    'chai','tailwind','theme'
 ]
-
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS =['127.0.0.1']
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -111,7 +112,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
