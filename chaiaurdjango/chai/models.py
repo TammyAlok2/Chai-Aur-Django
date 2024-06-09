25bbfa29bdefc1663c75fdf23b568a17e9bf473f
Alok Tamrakar <109581769+TammyAlok2@users.noreply.github.com>
2024-06-09 15:12:31 +0530

    Some admin model intialized

--- chaiaurdjango/chai/models.py
+++ chaiaurdjango/chai/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class ChaiVarity(models.Model):
+    CHAI_TYPES_CHOICE = {
+        {'ML','MASALA'},
+        {'GF','ANKITA'},
+    }
+    name = models.CharField(max_length=100)
+    image_field = models.ImageField(upload_to='chai/')
+    date_added = models.DateTimeField(default=timezone.now)
+    type= models.CharField(max_length=2,choices=CHAI_TYPES_CHOICE)
